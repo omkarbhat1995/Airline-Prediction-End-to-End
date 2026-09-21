9e378c0e3ab02c07d1ae9ff3d657e3dd1cf416e6
Omkar Bhat <omkarbhat1995@gmail.com>
2026-09-21 11:37:54 +0530

    uploads

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import  List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+        this funcion will return the list of the requirements
+
+    '''
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+        return requirements
+
+setup(
+    name="Airline Prediction",
+    version='0.0.1',
+    author='Omkar Bhat',
+    author_email='omkarbhat1995@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+
+)
