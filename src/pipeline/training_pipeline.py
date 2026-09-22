35c6187cc29f8e4c4ced3b8992927e9a531c9034
Omkar Bhat <omkarbhat1995@gmail.com>
2026-09-22 09:04:38 +0530

    removing cleaning and editing training pipleline

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -1,6 +1,6 @@
-from src.components.Step_01_Data_cleaning import Cleaning
+from src.components.Step_01_Cleaning import cleaning
 
-if __name__ =='__main__':
-    cleaning_obj = Cleaning()
-    df = cleaning_obj.read_data(filepath = 'G://Ethans//ML//Projects//Final Projects//Airline-Prediction-End-to-End//Artifact//Data//Airline1.csv')
+if __name__ == "__main___":
+    cleaningObj = cleaning()
+    df = cleaningObj.readData()
     print(df)
